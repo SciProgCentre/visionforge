30fa35bef5a1f0ef48358c67991c7835e3eebb72
Alexander Nozik <altavir@gmail.com>
2022-01-07 20:01:52 +0300

    Add lock file

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,11 +32,3 @@ apiValidation {
 }
 
 readme.readmeTemplate = file("docs/templates/README-TEMPLATE.md")
-
-//workaround for https://youtrack.jetbrains.com/issue/KT-48273
-rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
-    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().versions.apply {
-        webpack.version = "5.64.3"
-        webpackDevServer.version = "4.5.0"
-    }
-}
