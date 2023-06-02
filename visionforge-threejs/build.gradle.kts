23cb8765b66fdbf504d496db0cac8b41deb55f19
Alexander Nozik <altavir@gmail.com>
2023-06-02 14:33:41 +0300

    Minor fixes

--- visionforge-threejs/build.gradle.kts
+++ visionforge-threejs/build.gradle.kts
@@ -1,17 +1,23 @@
 plugins {
-    id("space.kscience.gradle.js")
+    id("space.kscience.gradle.mpp")
 }
 
 kotlin{
     explicitApi = org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode.Disabled
+}
+
+kscience{
     js{
         binaries.library()
     }
+    jsMain{
+        dependencies {
+            api(projects.visionforgeSolid)
+            implementation(npm("three", "0.143.0"))
+            implementation(npm("three-csg-ts", "3.1.10"))
+            implementation(npm("three.meshline","1.4.0"))
+        }
+    }
 }
 
-dependencies {
-    api(projects.visionforgeSolid)
-    implementation(npm("three", "0.143.0"))
-    implementation(npm("three-csg-ts", "3.1.10"))
-    implementation(npm("three.meshline","1.4.0"))
-}
+
