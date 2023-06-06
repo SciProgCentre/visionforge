a695e5e7c9da730a41ee97049f6c67ed6a702f5e
Alexander Nozik <altavir@gmail.com>
2023-06-06 09:52:46 +0300

    Use K2

--- visionforge-gdml/build.gradle.kts
+++ visionforge-gdml/build.gradle.kts
@@ -10,6 +10,7 @@ kscience {
     dependencies {
         api(projects.visionforgeSolid)
         api("space.kscience:gdml:0.5.0")
+        implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
     }
     dependencies(jvmTest) {
         implementation(spclibs.logback.classic)
