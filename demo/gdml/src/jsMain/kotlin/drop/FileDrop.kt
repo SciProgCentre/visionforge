2578fd6f77faa33080e644a6beb54a251c60aa79
Alexander Nozik <altavir@gmail.com>
2023-10-18 11:31:11 +0300

    Replace external enum with external sealed object

--- demo/gdml/src/jsMain/kotlin/drop/FileDrop.kt
+++ demo/gdml/src/jsMain/kotlin/drop/FileDrop.kt
@@ -9,30 +9,38 @@ import react.Component
 import react.Props
 import react.State
 
-external enum class DropEffects {
-    copy,
-    move,
-    link,
-    none
+sealed external class DropEffects {
+    @JsName("copy")
+    object Copy : DropEffects
+
+    @JsName("move")
+    object Move : DropEffects
+
+    @JsName("link")
+    object Link : DropEffects
+
+    @JsName("none")
+    object None : DropEffects
 }
 
-external interface FileDropProps: Props {
+external interface FileDropProps : Props {
     var className: String?
     var targetClassName: String?
     var draggingOverFrameClassName: String?
     var draggingOverTargetClassName: String?
 
-//    var frame?: Exclude<HTMLElementTagNameMap[keyof HTMLElementTagNameMap], HTMLElement> | HTMLDocument;
+    //    var frame?: Exclude<HTMLElementTagNameMap[keyof HTMLElementTagNameMap], HTMLElement> | HTMLDocument;
     var onFrameDragEnter: ((event: DragEvent) -> Unit)?
     var onFrameDragLeave: ((event: DragEvent) -> Unit)?
     var onFrameDrop: ((event: DragEvent) -> Unit)?
-//    var onDragOver: ReactDragEventHandler<HTMLDivElement>?
+
+    //    var onDragOver: ReactDragEventHandler<HTMLDivElement>?
 //    var onDragLeave: ReactDragEventHandler<HTMLDivElement>?
     var onDrop: ((files: FileList?, event: dynamic) -> Unit)?//event:DragEvent<HTMLDivElement>)
     var dropEffect: DropEffects?
 }
 
-external interface FileDropState: State {
+external interface FileDropState : State {
     var draggingOverFrame: Boolean
     var draggingOverTarget: Boolean
 }
